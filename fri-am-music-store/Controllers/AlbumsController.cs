161fec6064765c3a64682f94359388342dd63285
Rodrigo Lima Camargo Lima <200380021@student.georgianc.on.ca>
2018-09-21 09:44:07 -0400

    Add project files.

---
+++ fri-am-music-store/Controllers/AlbumsController.cs
@@ -0,0 +1,30 @@
+﻿using fri_am_music_store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Models
+{
+    public class AlbumsController : Controller
+    {
+        // GET: Albums
+        public ActionResult Index()
+        {
+            var albums = new List<Album>();
+
+            //Make a mock list of the albums
+            for (int i = 1; i <= 10; i++)
+            {
+                albums.Add(new Album { Title = "Album " + i.ToString() });
+            }
+
+            //pass the mock date to the view
+            //ViewBag.albums = albums;
+
+            //pass the strongly-typed album list directyl to the view
+            return View(albums);
+        }
+    }
+}
---
+++ fri-am-music-store/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fri_am_music_store.Models
+{
+    public class Album
+    {
+
+       public string Title { get; set; }
+    }
+}
